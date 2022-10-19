45c3e5dcc41336d06c416f2d264d9e1c5e7a5fd3
KVGFFN <kaine.vangeffen@gmail.com>
2022-10-19 16:41:20 +0200

    remove models.py

--- modaforge_workspace/modaforge/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class testModel(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
