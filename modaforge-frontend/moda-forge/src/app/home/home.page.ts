0c9fff7f1edaf023ad8be2bf83ae35ca56494d76
KVGFFN <kaine.vangeffen@gmail.com>
2022-10-26 14:55:04 +0200

    - modaforge dingen

--- modaforge-frontend/moda-forge/src/app/home/home.page.ts
+++ modaforge-frontend/moda-forge/src/app/home/home.page.ts
@@ -10,6 +10,13 @@ export class HomePage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.getAllUsers()
+  }
+
+  async getAllUsers() {
+    const response = await fetch('http://localhost:3000/users');
+    const data = await response.json();
+    console.log(data);
   }
 
 }
