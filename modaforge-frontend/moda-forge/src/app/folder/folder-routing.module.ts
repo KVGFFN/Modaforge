51c4f46587f0c79c89d99ce99733efcb9e81da89
KVGFFN <kaine.vangeffen@gmail.com>
2022-10-26 13:17:02 +0200

    route test

--- modaforge-frontend/moda-forge/src/app/folder/folder-routing.module.ts
+++ modaforge-frontend/moda-forge/src/app/folder/folder-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { FolderPage } from './folder.page';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: FolderPage
-  }
+    path: 'home',
+    component: HomeComponent
+  },
+  
 ];
 
 @NgModule({
