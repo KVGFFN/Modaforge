4d38bb5ecc3ffe992aa2693f251e48bdd088d062
Alan <s124568@ap.be>
2022-10-26 14:04:46 +0200

    added pages in side menu

--- modaforge-frontend/moda-forge/src/app/app.component.ts
+++ modaforge-frontend/moda-forge/src/app/app.component.ts
@@ -8,7 +8,11 @@ export class AppComponent {
   public appPages = [
     { title: 'Home', url: '/home', icon: 'home' },
     { title: 'Library', url: '/library', icon: 'cube' },
-    { title: 'Feedback', url: '/folder/Feedback', icon: 'cube' },
+    //{ title: 'Feedback', url: '/folder/Feedback', icon: 'cube' },
+    { title: 'Print posts', url: '/print-posts', icon: 'cube' },
+    { title: 'Printers', url: '/printers', icon: 'cube' },
+    { title: 'Near you', url: '/near-you', icon: 'cube' },
+    { title: 'Your prints', url: '/your-prints', icon: 'cube' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
