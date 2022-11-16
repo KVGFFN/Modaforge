21f517d3ed422461bb2d699070a041cd919013d0
KVGFFN <kaine.vangeffen@gmail.com>
2022-11-16 14:30:23 +0100

    add: Model

---
+++ ModaForgeAPI/ModaForge/ModaForge.Domain/Model.cs
@@ -0,0 +1,9 @@
+namespace ModaForge.Domain;
+
+public class Model
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int UserId { get; set; }
+    public string FileURL { get; set; }
+}
