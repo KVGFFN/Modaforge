d536636f328adcbb65338666fd505e2974575828
Arci <55528226+AjdinGaco@users.noreply.github.com>
2022-11-16 15:29:27 +0100

    Clean models code

--- ModaForgeAPI/ModaForge/ModaForge.Domain/Bridges/Tag_Model.cs
+++ ModaForgeAPI/ModaForge/ModaForge.Domain/Bridges/Tag_Model.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ModaForge.Domain.Bridges
+﻿namespace ModaForge.Domain.Bridges
 {
     public class Tag_Model
     {
--- ModaForgeAPI/ModaForge/ModaForge.Domain/Tag.cs
+++ ModaForgeAPI/ModaForge/ModaForge.Domain/Tag.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ModaForge.Domain
+﻿namespace ModaForge.Domain
 {
     public class Tag
     {
