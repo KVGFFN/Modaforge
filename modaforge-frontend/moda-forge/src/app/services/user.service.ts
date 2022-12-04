267a4ce6516856ac01af38b69970d5f02e91cb5f
KVGFFN <kaine.vangeffen@gmail.com>
2022-12-04 14:44:33 +0100

    add GET & POST in user

--- modaforge-frontend/moda-forge/src/app/services/user.service.ts
+++ modaforge-frontend/moda-forge/src/app/services/user.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { IP } from 'src/helpers/IP';
+import { HttpClient } from '@angular/common/http';
+import { User } from 'src/modules/interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  API = IP.local;
+
+  // Get all users
+  getAllUsers()
+  {
+    return this.http.get<User[]>(`${this.API}/User`);
+  }
+
+  // Add user
+  addUser(user: User)
+  {
+    return this.http.post<User[]>(`${this.API}/User`, {user});
+  }
+
 }
