c88cda48f243979b4e008a7db48d44017337080f
KVGFFN <kaine.vangeffen@gmail.com>
2022-12-04 14:01:56 +0100

    add: user.interface.ts

---
+++ modaforge-frontend/moda-forge/src/modules/interfaces/user.interface.ts
@@ -0,0 +1,14 @@
+export interface User {
+    id:       number;
+    name:     string;
+    verified: boolean;
+    email:    string;
+    regionId: number;
+    region:   Region;
+}
+
+export interface Region {
+    id:      number;
+    name:    string;
+    zipCode: number;
+}
