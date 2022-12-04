c39f870eaaaa502877d2ab9d5df2688f68a8be62
KVGFFN <kaine.vangeffen@gmail.com>
2022-12-04 15:51:39 +0100

    fix: HTTP POST of user

--- modaforge-frontend/moda-forge/src/app/services/user.service.ts
+++ modaforge-frontend/moda-forge/src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { IP } from 'src/helpers/IP';
 import { HttpClient } from '@angular/common/http';
 import { User } from 'src/modules/interfaces/user.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -21,9 +22,11 @@ export class UserService {
   }
 
   // Add user
-  addUser(user: User)
+  addUser(user: User): Observable<User>
   {
-    return this.http.post<User[]>(`${this.API}/User`, {user});
+    const headers = { 'content-type': 'application/json'}
+    const body = JSON.stringify(user);
+    return this.http.post<User>(`${this.API}/User`, body, {'headers': headers});
   }
 
 }
