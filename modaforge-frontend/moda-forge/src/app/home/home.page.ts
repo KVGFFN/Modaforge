91a30ccab54a51b4b83ea2fe3e68f5d548c58bb6
KVGFFN <kaine.vangeffen@gmail.com>
2022-12-04 15:52:49 +0100

    MAJOR: registered user now ends up in database

--- modaforge-frontend/moda-forge/src/app/home/home.page.ts
+++ modaforge-frontend/moda-forge/src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { FirebaseApp, initializeApp } from "firebase/app";
 import { environment } from 'src/environments/environment';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -14,21 +15,33 @@ export class HomePage implements OnInit {
   
 
 
-  constructor() { 
+  constructor(private userService: UserService) 
+  {
     this.users = [];
   }
 
   ngOnInit() {
     this.getAllUsers()
+
   }
 
   
 
+  // async getAllUsers() {
+  //   const response = await fetch('https://localhost:7271/api/User', {method: 'GET'});
+  //   const data = await response.json();
+  //   console.log(data);
+  //   this.users = data;
+  // }
+
   async getAllUsers() {
-    const response = await fetch('https://localhost:7271/api/User', {method: 'GET'});
-    const data = await response.json();
-    console.log(data);
-    this.users = data;
+    this.userService.getAllUsers().subscribe((data: any) => {
+      console.log(data);
+      this.users = data;
+    });
   }
+
+
+  
 
 }
--- modaforge-frontend/moda-forge/src/app/login/login.page.ts
+++ modaforge-frontend/moda-forge/src/app/login/login.page.ts
@@ -7,6 +7,9 @@ import { loginHelper } from '../loginHelper';
 import { environment } from 'src/environments/environment';
 import { NgModel } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+import { UserService } from '../services/user.service';
+import { User } from 'src/modules/interfaces/user.interface';
+import { Region } from 'src/modules/interfaces/user.interface';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -22,11 +25,31 @@ export class LoginPage implements OnInit {
   _loginhelper = loginHelper;
   user: any;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private userService: UserService) {}
 
+  // User
   name: string;
   email: string;
   password: string;
+
+  // Region
+  regionName: string;
+  regionZipcode: number;
+
+  USER_DATA: User = {
+    id: undefined,
+    name: undefined,
+    verified: undefined,
+    email: undefined,
+    regionId: undefined,
+    region: undefined
+  }
+
+  REGION_DATA: Region = {
+    id: undefined,
+    name: undefined,
+    zipcode: undefined
+  }
 
   async updateUserName(name: string)
   {
@@ -41,7 +64,7 @@ export class LoginPage implements OnInit {
     });
   }
 
-  login()
+  register()
   {
     console.log("EXECUTED LOGIN METHOD LOGIN.PAGE.TS");
     createUserWithEmailAndPassword(this.auth, this.email, this.password)
@@ -52,13 +75,31 @@ export class LoginPage implements OnInit {
       console.log("NAME: " + this.user.displayName);
       console.log("EMAIL: " + this.user.email);
 
+      // Add user to database
+
+      this.REGION_DATA.id = 0;
+      this.REGION_DATA.name = this.regionName;
+      this.REGION_DATA.zipcode = this.regionZipcode;
+
+      this.USER_DATA.id = 0;
+      this.USER_DATA.email = this.email;
+      this.USER_DATA.name = this.name
+      this.USER_DATA.verified = false;
+      this.USER_DATA.regionId = 0;
+      this.USER_DATA.region = this.REGION_DATA;
+
+      
+      this.userService.addUser(this.USER_DATA).subscribe((data: any) => {
+        console.log("--> userService.addUser login.page.ts:77")
+        console.log(data);
+      });
       this.router.navigate(['/home']);
       
     })
     
     .catch((error) => {
-      const errorCode = error.code;
       const errorMessage = error.message;
+      console.log("ERROR: " + errorMessage);
     });
 
   }
