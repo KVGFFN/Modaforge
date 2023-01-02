b4349ad6ec3dec4655fd298d2c097ad3ec8a6d46
Alan <s124568@ap.be>
2023-01-02 17:36:38 +0100

    add: test configuration for post

---
+++ ModaForgeAPI/ModaForge/ModaForge.Infrastructure/Configurations/PostConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ModaForge.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModaForge.Infrastructure.Configurations
+{
+    public class PostConfiguration : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.HasData(
+                new Post
+                {
+                    Id = 1,
+                    Name = "testPost",
+                    Created = DateTime.Now,
+                    UserId = 420,
+                }
+            );
+        }
+    }
+}
