804ed4f884d5742686f62a85e5df330bcb711c6e
KVGFFN <kaine.vangeffen@gmail.com>
2023-01-03 20:59:11 +0100

    shorthand code for register() method

--- modaforge-frontend/moda-forge/src/app/register/register.page.ts
+++ modaforge-frontend/moda-forge/src/app/register/register.page.ts
@@ -30,7 +30,7 @@ export class RegisterPage implements OnInit {
   apistate = APIstate.isActive;
   IP = IP.local;
 
-  constructor(private router: Router, private userService: UserService) {}
+  constructor(private router: Router, private userService: UserService) { }
 
   // Register check
   userExists: boolean = false;
@@ -45,88 +45,80 @@ export class RegisterPage implements OnInit {
   regionZipcode: number;
 
   USER_DATA: User = {
-    id:       undefined,
-    name:     undefined,
+    id: undefined,
+    name: undefined,
     verified: undefined,
-    email:    undefined,
-    picture:  undefined,
+    email: undefined,
+    picture: undefined,
     regionId: undefined,
-    region:   undefined
+    region: undefined
   }
 
   REGION_DATA: Region = {
-    id:       undefined,
-    name:     undefined,
-    zipcode:  undefined
+    id: undefined,
+    name: undefined,
+    zipcode: undefined
   }
 
-  async updateUserName(name: string)
-  {
+  async updateUserName(name: string) {
     this.user = this.auth.currentUser;
     await updateProfile(this.user, {
       displayName: name
     }).then(() => {
       console.log("Name updated to " + name);
     })
-    .catch((error) => {
-      console.log(error);
-    });
-  }
-
-  register()
-  {
-    console.log("EXECUTED LOGIN METHOD LOGIN.PAGE.TS");
-
-    // Check if user with email already exists
-    createUserWithEmailAndPassword(this.auth, this.email, this.password)
-    .then((userCredential) => {
-      this.user = userCredential.user;
-      this.updateUserName(this.name);
-      console.log("VV USERINFO VV");
-      console.log("NAME: " + this.user.displayName);
-      console.log("EMAIL: " + this.user.email);
-
-      // Add user to database
-
-      this.REGION_DATA.id = 0;
-      this.REGION_DATA.name = this.regionName;
-      this.REGION_DATA.zipcode = this.regionZipcode;
-
-      this.USER_DATA.id = 0;
-      this.USER_DATA.email = this.email;
-      this.USER_DATA.name = this.name
-      this.USER_DATA.verified = false;
-      this.USER_DATA.picture = "https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"; // default picture
-      this.USER_DATA.regionId = 0;
-      this.USER_DATA.region = this.REGION_DATA;
-
-
-      this.userService.addUser(this.USER_DATA).subscribe((data: any) => {
-        console.log("--> userService.addUser register.page.ts:77")
-        console.log(data);
+      .catch((error) => {
+        console.log(error);
       });
-      this.router.navigate(['/home']);
+  }
 
-    })
+  register() {
+    console.log('%cregister() in register.page.ts', 'color: yellow');
 
-    .catch((error) => {
-      const errorMessage = error.message;
-      console.log("ERROR: " + errorMessage);
-      alert("ERROR: " + errorMessage);
-    });
+    createUserWithEmailAndPassword(this.auth, this.email, this.password)
+      .then(userCredential => {
+        this.user = userCredential.user;
+        this.updateUserName(this.name);
+        console.log('VV USERINFO VV');
+        console.log(`NAME: ${this.user.displayName}`);
+        console.log(`EMAIL: ${this.user.email}`);
 
+        // Add user to database
+        this.USER_DATA = {
+          id: 0,
+          email: this.email,
+          name: this.name,
+          verified: false,
+          picture: 'https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png', // default picture
+          regionId: 0,
+          region: {
+            id: 0,
+            name: this.regionName,
+            zipcode: this.regionZipcode
+          }
+        };
+
+        this.userService.addUser(this.USER_DATA).subscribe((data: any) => {
+          console.log('--> userService.addUser register.page.ts:77');
+          console.log(data);
+        });
+        this.router.navigate(['/home']);
+      })
+      .catch(error => {
+        const errorMessage = error.message;
+        console.log(`ERROR: ${errorMessage}`);
+        alert(`ERROR: ${errorMessage}`);
+      });
   }
 
 
-  ngOnInit()
-  {
-    if (this.user)
-    {
+
+  ngOnInit() {
+    if (this.user) {
       console.log(">> USER IS LOGGED IN");;
       this.router.navigate(['/home']);
     }
-    else
-    {
+    else {
       console.log(">> USER IS NOT LOGGED IN");
       this.router.navigate(['/register']);
     }
