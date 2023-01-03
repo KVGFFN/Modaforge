39970e99066484e39df26b70475590dd579a0ec7
KVGFFN <kaine.vangeffen@gmail.com>
2023-01-03 22:08:50 +0100

    edit: request.interface.ts (temporarily commented out missing interfaces)

--- modaforge-frontend/moda-forge/src/modules/interfaces/request.interface.ts
+++ modaforge-frontend/moda-forge/src/modules/interfaces/request.interface.ts
@@ -1,9 +1,24 @@
+import { Model } from "./model.interface";
+
+
 export interface Request
 {
-    "id": number;
-    "title": string;
-    "description": string;
-    "creationDate": string;
-    "acceptedDate": string;
-    "doneDate": string;
+    id: number;
+    title: string;
+    description: string;
+    status: number;
+    creationDate: Date;
+    acceptedDate?: Date;
+    doneDate?: Date;
+
+    requesterId?: number;
+    providerId?: number;
+    modelId?: number;
+    regionId?: number;
+
+    // requester?: Requester;
+    // provider?: Provider;
+    // model?: Model;
+    // region?: Region;
+
 }
