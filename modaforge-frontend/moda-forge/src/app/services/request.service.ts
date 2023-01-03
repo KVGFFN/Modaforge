8531334c5c1d783d73188569158ef832b7d88b34
KVGFFN <kaine.vangeffen@gmail.com>
2023-01-03 21:49:38 +0100

    add: temporary code

--- modaforge-frontend/moda-forge/src/app/services/request.service.ts
+++ modaforge-frontend/moda-forge/src/app/services/request.service.ts
@@ -1,9 +1,54 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IP } from 'src/helpers/IP';
+import { Model } from 'src/modules/interfaces/model.interface';
+import { Region } from 'src/modules/interfaces/user.interface';
+
+// export interface Request 
+// {
+//   id: number;
+//   title: string;
+//   description: string;
+//   status: number;
+//   creationDate: Date;
+//   acceptedDate?: Date;
+//   doneDate?: Date;
+
+//   requesterId?: number;
+//   providerId?: number;
+//   modelId?: number;
+
+//   regionId?: number;
+
+//   requester?: Requester;
+//   provider?: Provider;
+//   model?: Model;
+//   region?: Region;
+
+// }
 
 @Injectable({
   providedIn: 'root'
 })
-export class RequestService {
 
-  constructor() { }
+export class RequestService {
+  
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  API = IP.local;
+
+  // createRequest(request: Request): Observable<Request>
+  // {
+  //   const headers = { 'content-type': 'application/json'}
+  //   const body = JSON.stringify(request);
+  //   return this.http.post<Request>(`${this.API}/api/Request`, body, {'headers': headers});
+  // }
+
+
+
 }
+
