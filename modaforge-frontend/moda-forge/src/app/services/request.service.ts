390cedb6fa0ec6ebcfca1c0fd63b6c19ba2ceec5
KVGFFN <kaine.vangeffen@gmail.com>
2023-01-12 14:12:33 +0100

    add: createRequest for request.service

--- modaforge-frontend/moda-forge/src/app/services/request.service.ts
+++ modaforge-frontend/moda-forge/src/app/services/request.service.ts
@@ -16,17 +16,17 @@ export class RequestService {
 
   API = IP.local;
 
-  // createRequest(request: Request): Observable<Request>
-  // {
-  //   const headers = { 'content-type': 'application/json'}
-  //   const body = JSON.stringify(request);
-  //   return this.http.post<Request>(`${this.API}/api/Request`, body, {'headers': headers});
-  // }
-
   // Get request by id
   getRequest(id: number): Observable<Request>
   {
     return this.http.get<Request>(`${this.API}/api/Request/${id}`);
+  }
+
+  createRequest(request: Request): Observable<Request>
+  {
+    const headers = { 'content-type': 'application/json'}
+    const body = JSON.stringify(request);
+    return this.http.post<Request>(`${this.API}/api/Request`, body, {'headers': headers});
   }
 
 
