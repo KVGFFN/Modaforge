37f8e86dec0b5be395ebe14ad7ca37a4cc5e2c83
Alan <s124568@ap.be>
2023-01-12 17:45:52 +0100

    add: toggleCreatePost form

--- modaforge-frontend/moda-forge/src/app/print-posts/print-posts.page.ts
+++ modaforge-frontend/moda-forge/src/app/print-posts/print-posts.page.ts
@@ -7,12 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrintPostsPage implements OnInit {
 
+   // Variables
+   posts: any[] = ["test", "test2", "test3", "test4", "test5"];
+   showCreatePostForm: boolean = false;
+
   constructor() { }
 
-  // Variables
-  posts: any[] = ["test", "test2", "test3", "test4", "test5"];
-
   ngOnInit() {
+  }
+
+  toggleCreatePostForm() {
+    this.showCreatePostForm = !this.showCreatePostForm;;
   }
 
 }
