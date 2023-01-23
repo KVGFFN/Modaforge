dc11ae5b167aad06aee6d0bbfc3f9f6718d97726
Ajdin Gaco <ajdin.gaco@student.ap.be>
2023-01-24 00:55:28 +0100

    Fix PageSize

--- ModaForgeAPI/ModaForge/ModaForge.Domain/SearchParameters.cs
+++ ModaForgeAPI/ModaForge/ModaForge.Domain/SearchParameters.cs
@@ -9,7 +9,7 @@ namespace ModaForge.Domain
     public class SearchParameters
     {
         public int PageNumber { get; set; } = 1;
-        public int PageSize = 20;
+        public int PageSize { get; set; } = 20;
 
         public string? Tags { get; set; }
         public string? Keyword { get; set; }
