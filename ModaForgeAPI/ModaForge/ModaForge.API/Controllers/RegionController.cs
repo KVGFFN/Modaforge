f53d37490571c54139a03dbe19398952e17038f0
Alan <s124568@ap.be>
2023-01-25 02:06:52 +0100

    add RegionController and IRegionService

---
+++ ModaForgeAPI/ModaForge/ModaForge.API/Controllers/RegionController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModaForge.Domain;
+using ModaForge.Application.Inferfaces.Service;
+using ModaForge.Application.Inferfaces.IService;
+
+namespace ModaForge.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionController : ControllerBase
+    {
+        private IRegionService service;
+        public RegionController(IRegionService service)
+        {
+            this.service = service;
+        }
+        [HttpGet]
+        public IActionResult GetAllRegions([FromQuery] SearchParameters searchParameters)
+        {
+            return Ok(service.GetAll(searchParameters));
+        }
+        [Route("{id}")]
+        [HttpGet]
+        public IActionResult GetRegion([FromRoute] int id)
+        {
+            var region = service.GetById(id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+            return Ok(region);
+        }
+        [HttpPost]
+        public IActionResult CreateRegion([FromBody] Region region)
+        {
+            try
+            {
+                var newRegion = service.Create(region);
+                return Ok(newRegion);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [Route("{id}")]
+        [HttpPut]
+        public IActionResult UpdateRegion([FromRoute] int id, [FromBody] Region region)
+        {
+            return Ok(service.Update(id, region));
+        }
+        [Route("{id}")]
+        [HttpDelete]
+        public IActionResult DeleteRegion([FromRoute] int id)
+        {
+            service.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ ModaForgeAPI/ModaForge/ModaForge.Application/Inferfaces/IService/IRegionService.cs
@@ -0,0 +1,18 @@
+﻿using ModaForge.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModaForge.Application.Inferfaces.IService
+{
+    public interface IRegionService
+    {
+        Region Create(Region region);
+        void Delete(Region region);
+        IEnumerable<Region> GetAll(SearchParameters searchParameters);
+        Region GetById(int id);
+        Region Update(int id, Region region);
+    }
+}
